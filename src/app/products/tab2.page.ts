ff5c88b7098c6902488dd8e5753e0212a3d776eb
PieroLeon <pierolh2000@hotmail.com>
2024-04-30 23:12:03 -0500

    ProjectDeliveryKiosko

--- src/app/products/tab2.page.ts
+++ src/app/products/tab2.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  myData: any; 
 
-  constructor() {}
+  products = [
+    {
+      name: 'Galleta Soda',
+      description: 'Galleta de soda hecho con productos premium',
+      image: 'https://placehold.co/300x200',
+      price: 'S/1.5'
+    },
+    {
+      name: 'Galleta Soda',
+      description: 'Galleta de soda hecho con productos premium',
+      image: 'https://placehold.co/300x200',
+      price: 'S/1.5'
+    }
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void {this.myData = 'Datos inicializados';
+  }
+
+  updateProduct(product: any) {
+    console.log('Actualizar producto:', product);
+  }
+
+  deleteProduct(product: any) {
+    console.log('Borrar producto:', product);
+  }
+
+  addProduct() {
+    console.log('Agregar producto');
+  }
 }
+
+
--- src/app/update/update.page.ts
+++ src/app/update/update.page.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-update',
   templateUrl: './update.page.html',
   styleUrls: ['./update.page.scss'],
 })
 export class UpdatePage implements OnInit {
+  myData: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.myData = 'Datos inicializados';
   }
 
 }
