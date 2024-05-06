e3bdafe100099031a18f6f134d6aa56f56e93fa5
KanehiroSW <kanehirosw@gmail.com>
2024-05-05 22:17:09 -0500

    Por fin TUSI

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { SelectChangeEventDetail } from '@ionic/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  selectedRole: string = '';
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  onRoleChange(event: CustomEvent<SelectChangeEventDetail>) {
+    this.selectedRole = event.detail.value;
+  }
+  
+  onLogin() {
+    if (this.selectedRole === 'tienda') {
+      this.router.navigateByUrl('/tabs/tab1');
+    } else if (this.selectedRole === 'repartidor') {
+      this.router.navigateByUrl('/order-deliman');
+    } else {
+    }
+  }
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
+  fotoTomada: string | undefined;
   constructor() { }
 
   ngOnInit() {
   }
+  async tomarFoto() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Base64,
+      source: CameraSource.Camera
+    });
+
+    this.fotoTomada = 'data:image/jpeg;base64,' + image.base64String;
+  }
+
 
 }
